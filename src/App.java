eb5c59c5cfd89de6955d8a90b0be1b10c0ec66cd
Cogee <cogansam@gmail.com>
2020-12-09 20:03:38 +0000

    example code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!!!!");
+    public static void main(String[] args) {
+        //nbcbs!
+        StringBuffer strBuff=new StringBuffer();
+        String word="NCIS";
+        String merged;
+        char[] otherWord={'B','o','b','!'};
+        for(int i=0;i<=otherWord.length-1;i++){
+            if(word.charAt(i)!='I' && word.charAt(i)!='o'){
+                strBuff.append(word.charAt(i));
+            }
+
+
+            if(otherWord[i]!='I' && otherWord[i]!='o'){
+                strBuff.append(otherWord[i]);
+            }
+            
+        }
+        merged=strBuff.toString();
+        System.out.println(merged);
     }
 }
